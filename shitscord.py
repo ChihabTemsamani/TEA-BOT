e73ff30e9e1febfd09543d65b353c15bc47fbd36
TheLoneWolf316 <TheLoneWolf316@users.noreply.github.com>
2018-01-20 21:53:55 -0500

    Bot mostly functional.
    Small stuff like apathy towards caps need fixing. Bot commands and actions must be added now

--- shitscord.py
+++ shitscord.py
@@ -14,11 +14,22 @@ client = commands.Bot(command_prefix = "?")
 async def on_ready():
     print("Bot is online and connected to Discord") #When Bot Connects
 
+commands = [] #All available commands go here
+
 @client.event
 async def on_message(message):
     if message.content.upper().startswith('!SHITSCORD'):
         print("I Have Been Summoned")
-        print("User States:", message.content)
-        await client.send_message(message.channel, ":shit:")
+        command = message.content.split(" ")
+        print("User States:", command)
+
+        #checks if input is valid command with weird double if else and acts accordingly
+        if len(command) > 1:
+            if command[1] in commands: 
+                await client.send_message(message.channel, "Thats a command") #If command is correct (all command actions (most of bot) go here)
+            else:
+                await client.send_message(message.channel, ":shit:")# If command is incorrect
+        else:
+            await client.send_message(message.channel, ":shit:") #If no command is given
 
 client.run(token)
