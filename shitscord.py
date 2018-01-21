0c3fe9e2cd70af15067107dd9978376c91901141
TheLoneWolf316 <TheLoneWolf316@users.noreply.github.com>
2018-01-21 18:17:23 -0500

    Set up OS var for Heroku

--- shitscord.py
+++ shitscord.py
@@ -8,7 +8,8 @@ from copypasta import * #yes this is bad
 try:
 	from discord_token import token #Token stored in file called discord_token.py as var token. This is a try loop so the program can run locally or when deployed
 except: 
-	print("Token file not found. Hoping variable defined")
+	print("Token file not found. Trying to import OS")
+	from os import token
 #Client loaded and connected
 Client=discord.Client()
 client=commands.Bot(command_prefix = "?") 
