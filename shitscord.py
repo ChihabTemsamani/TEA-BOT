9584dbf304089895e86d96080f28aeb720af55d2
TheLoneWolf316 <TheLoneWolf316@users.noreply.github.com>
2018-01-21 20:00:41 -0500

    the script can now d o t  h   e     m        o        d         u          l                e

--- shitscord.py
+++ shitscord.py
@@ -3,17 +3,18 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
-from copypasta import * #yes this is bad
+from copypasta_content import * #yes this is bad
+from copypasta import copypastascript
 
 try:
-	from discord_token import token #Token stored in file called discord_token.py as var token. This is a try loop so the program can run locally or when deployed
+    from discord_token import token #Token stored in file called discord_token.py as var token. This is a try loop so the program can run locally or when deployed
 except: 
-	print("Token file not found. Trying to import OS")
-	import os
-	token=os.environ.get('token')
+    print("Token file not found. Trying to import OS")
+    import os
+    token=os.environ.get('token')
 #Client loaded and connected
 Client=discord.Client()
-client=commands.Bot(command_prefix = "?") 
+client=commands.Bot(command_prefix = "!") 
 
 
 @client.event 
@@ -33,23 +34,8 @@ async def on_message(message):
         #checks if input is valid command with weird double if else and acts accordingly
         if len(command) > 1:
             if command[1].lower() in commands: #If command is correct (all command actions (most of bot) go here)
-
-                #CopyPasta Serving
                 if command[1].lower()=="copypasta":
-                    if command[2].lower() == "list":
-                        print("Copypasta List Requested")
-                        cpserve=(" ".join(cpnames[0:])) #Get list of CP names
-                        await client.send_message(message.channel, cpserve) #Send List
-
-                        
-                    if command[2].lower() in cpnames:
-                        print("Copypasta Requested")
-                        cprequest=command[2].lower() #All lowercase name
-                        cplocate=cpnames.index(cprequest) #Get index number of CP
-                        cpserve=cplist[cplocate] #Get the CP
-                        await client.send_message(message.channel, cpserve) #Send CP
-
-                        
+                    cpfunction(command)
             else:
                 await client.send_message(message.channel, ":shit:")# If command is incorrect
         else:
