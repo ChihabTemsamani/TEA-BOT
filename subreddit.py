b91ce108c62ab5b8ca5a470215815c01a9706a13
TheLoneWolf316 <TheLoneWolf316@users.noreply.github.com>
2018-04-02 15:08:14 -0400

    Added detection of invalid subreddits to prevent crashes

--- subreddit.py
+++ subreddit.py
@@ -12,43 +12,52 @@ def subredditscript(command, client, message):
         reddit = praw.Reddit(client_id=reddit_config.client_id, client_secret=reddit_config.client_secret,
                              user_agent=reddit_config.user_agent)
     
+    #Check to make sure subreddit exists
+    try:
+        reddit.subreddits.search_by_name(srname, exact=True)
+    except:
+        print("Invalid Subreddit")
+        run_coro(client.send_message(message.channel, "Invalid Subreddit"), client)
+    else:
+        
     
-    #Find the max amount of posts in a subreddit, or whatever reddit tops out at (usually 100) in order to prevent index errors
-    #Max posts stored as postcount variable
-    postcount = 0
-    for _ in reddit.subreddit(srname).search('', sort='hot'):
-        postcount += 1
-    print("Post range = ",postcount)
-    
-    x=True
-    accepted_type= ["jpg","peg","png"] #last three characters of image file extensions
-    while x:
-        sub = reddit.subreddit(srname)
-        srposts = [post for post in sub.hot(limit=postcount)]
-        randpostnum = random.randint(0, postcount-1)
-        randpost = srposts[randpostnum]
-        url = str(randpost.url)
-        url_type=url[-3:]
-        print(url)
-        print(url_type)
-        if url_type in accepted_type:
-            x=False
-            print("URL is to an image file")
-    if url_type=="jpg":
-        file_name="images/"+str(random.randint(1,1000))+".jpg"
-    if url_type=="jpeg":
-        file_name="images/"+str(random.randint(1,1000))+".jpeg"
-    if url_type=="png":
-        file_name="images/"+str(random.randint(1,1000))+".png"
-    if url_type=="gif":
-        file_name="images/"+str(random.randint(1,1000))+".gif"            
-    print("Downloading image from: ", url)
-    urllib.request.urlretrieve(url, file_name)
-    open(file_name + ".lock","a").close()
-    print("Image placed at: ",file_name," and locked")
-    time.sleep(1)
-    print("Sending Image")
-    run_coro(client.send_file(message.channel, file_name), client) # Send Image
-    print("Sent Image")
-    os.remove(file_name + ".lock")
-    print(file_name, " unlocked.")
+        
+        #Find the max amount of posts in a subreddit, or whatever reddit tops out at (usually 100) in order to prevent index errors
+        #Max posts stored as postcount variable
+        postcount = 0
+        for _ in reddit.subreddit(srname).search('', sort='hot'):
+            postcount += 1
+        print("Post range = ",postcount)
+        
+        x=True
+        accepted_type= ["jpg","peg","png"] #last three characters of image file extensions
+        while x:
+            sub = reddit.subreddit(srname)
+            srposts = [post for post in sub.hot(limit=postcount)]
+            randpostnum = random.randint(0, postcount-1)
+            randpost = srposts[randpostnum]
+            url = str(randpost.url)
+            url_type=url[-3:]
+            print(url)
+            print(url_type)
+            if url_type in accepted_type:
+                x=False
+                print("URL is to an image file")
+        if url_type=="jpg":
+            file_name="images/"+str(random.randint(1,1000))+".jpg"
+        if url_type=="jpeg":
+            file_name="images/"+str(random.randint(1,1000))+".jpeg"
+        if url_type=="png":
+            file_name="images/"+str(random.randint(1,1000))+".png"
+        if url_type=="gif":
+            file_name="images/"+str(random.randint(1,1000))+".gif"            
+        print("Downloading image from: ", url)
+        urllib.request.urlretrieve(url, file_name)
+        open(file_name + ".lock","a").close()
+        print("Image placed at: ",file_name," and locked")
+        time.sleep(1)
+        print("Sending Image")
+        run_coro(client.send_file(message.channel, file_name), client) # Send Image
+        print("Sent Image")
+        os.remove(file_name + ".lock")
+        print(file_name, " unlocked.")
