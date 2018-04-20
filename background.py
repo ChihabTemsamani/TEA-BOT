a873c7b9e7ebee793a71628776aa3575bf7cfdc8
TheLoneWolf316 <TheLoneWolf316@users.noreply.github.com>
2018-04-19 22:37:12 -0400

    Fixed image clearing

--- background.py
+++ background.py
@@ -3,8 +3,11 @@ import time, os
 global run
 print("Background open")
 while True:
+    #This script runs every 5 minutes
     time.sleep(300)
     print("Clearing the images folder")
     for item in os.listdir("images"):
         if not item.endswith(".lock") and not os._exists(item + ".lock"):
-            os.remove(item)
+            filename = ("images/" + item)
+            print("Removing: ", filename)
+            os.remove(filename)
