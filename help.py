f801ccfbc7caef04dedee182cdde7ea22638b1cf
Sagan <saganm316@gmail.com>
2018-07-19 19:20:21 -0400

    added spam function

--- help.py
+++ help.py
@@ -1,10 +1,14 @@
-import constants
+import constants, time
 
 def script(command, client, message):
     constants.run_coro(client.send_message(message.channel, "I shitpost memes and copypastas"), client)
+    time.sleep(.25)
     constants.run_coro(client.send_message(message.channel, "Availible commands:"), client)
+    time.sleep(.25)
     commands_str = "`" + ", ".join(constants.cmdlist[0:]) + "`"
     constants.run_coro(client.send_message(message.channel, commands_str), client)
+    time.sleep(.25)
     constants.run_coro(client.send_message(message.channel, "You can end any command in `-p` to preserve your message."), client)
+    time.sleep(.25)
     constants.run_coro(client.send_message(message.channel, "https://github.com/Shitscord/Shitscord"), client)
     print("help info requested")
--- subreddit.py
+++ subreddit.py
@@ -11,9 +11,9 @@ def script(command, client, message):
     if command[1].lower() == "image":
         srname=command[2]
         print("Getting random image from",srname)
-        
+                
         #Sortby
-        if "-s" in command:
+        if "-s" in str(command).lower():
             sloc=command.index("-s")
             if len(command)>sloc+1 and str(command[sloc+1]).lower() in sortbymodes:
                 sortby=str(command[sloc+1]).lower()
@@ -23,7 +23,7 @@ def script(command, client, message):
             sortby="hot"
         
         #Range
-        if "-r" in command:
+        if "-r" in str(command).lower():
             rloc=command.index("-r")
             if len(command)>rloc+1 and int(command[rloc+1])>0 and int(command[rloc+1])<1000:
                 maxrange=int(command[rloc+1])
@@ -33,45 +33,61 @@ def script(command, client, message):
             maxrange=100    
     
         #Attempts
-        if "-a" in command:
+        if "-a" in str(command).lower():
             aloc=command.index("-a")
-            if len(command)>sloc+1 and int(command[aloc+1])>0 and int(command[aloc+1])<30:
+            if len(command)>aloc+1 and int(command[aloc+1])>0 and int(command[aloc+1])<30:
                 attempts=int(command[aloc+1])
             else:
                 attempts=5
         else:
             attempts=5
-      
-        url=rif.find(srname, sortby, maxrange, attempts)        
         
-        if url=="sr_invalid":
-            print("Invalid Subreddit")
-            constants.run_coro(client.send_message(message.channel, "That Subreddit does not exist."), client)
-            return
-
-        if url=="noimagefound":
-            print("No Image Found")
-            constants.run_coro(client.send_message(message.channel, "No image found in that Subreddit."), client)
-            return
-            
+        #Spam
+        if "-m" in str(command).lower():
+            spamloc=command.index("-m")
+            if len(command)>spamloc+1 or len(command)==spamloc+1 and int(command[spamloc+1])>0 and int(command[spamloc+1])<10:
+                print(command[spamloc+1])
+                spam=int(command[spamloc+1])
+            else:
+                spam=1
         else:
-            url_type=url[-3:]
-            print("Image Found")
-            if url_type=="jpg":
-                file_name="images/"+str(random.randint(1,1000))+".jpg"
-            if url_type=="jpeg":
-                file_name="images/"+str(random.randint(1,1000))+".jpeg"
-            if url_type=="png":
-                file_name="images/"+str(random.randint(1,1000))+".png"         
-            print("Downloading image from: ", url)
-            urllib.request.urlretrieve(url, file_name)
-            open(file_name + ".lock","a").close()
-            print("Image placed at: ",file_name," and locked")
-            time.sleep(1)
-            print("Sending Image")
-            constants.run_coro(client.send_file(message.channel, file_name), client) # Send Image
-            print("Sent Image")
-            os.remove(file_name + ".lock")
-            print(file_name, " unlocked.")
+            spam=1
+        
+        print("Spam:", spam)
+        x=0
+        while x<spam:
+            url=rif.find(srname, sortby, maxrange, attempts)  
+            
+        
+            if url=="sr_invalid":
+                print("Invalid Subreddit")
+                constants.run_coro(client.send_message(message.channel, "That Subreddit does not exist."), client)
+                return
+    
+            if url=="noimagefound":
+                print("No Image Found")
+                constants.run_coro(client.send_message(message.channel, "No image found in that Subreddit."), client)
+                return
+                
+            else:
+                url_type=url[-3:]
+                print("Image Found")
+                if url_type=="jpg":
+                    file_name="images/"+str(random.randint(1,1000))+".jpg"
+                if url_type=="jpeg":
+                    file_name="images/"+str(random.randint(1,1000))+".jpeg"
+                if url_type=="png":
+                    file_name="images/"+str(random.randint(1,1000))+".png"         
+                print("Downloading image from: ", url)
+                urllib.request.urlretrieve(url, file_name)
+                open(file_name + ".lock","a").close()
+                print("Image placed at: ",file_name," and locked")
+                time.sleep(1)
+                print("Sending Image")
+                constants.run_coro(client.send_file(message.channel, file_name), client) # Send Image
+                print("Sent Image")
+                os.remove(file_name + ".lock")
+                print(file_name, " unlocked.")
 
+            x=x+1
            
